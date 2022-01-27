cfbdaf19fd10a56ff0eec3afced163fb31addbcc
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-01-26 20:27:02 -0800

    connected the two pages but I keep running in to a bug git add .

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,7 @@
 var repnNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,9 +17,20 @@ var getRepoIssues = function (repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          document.location.replace("./index.html");
         }
       });
+};
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
 };
 
 var displayIssues = function(issues) {
@@ -69,4 +80,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
